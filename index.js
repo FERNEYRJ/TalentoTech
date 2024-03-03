3fc39d0dd6738408509141238b9cddfaa6bb0eb8
FERNEY ROJAS <46285449+FERNEYRJ@users.noreply.github.com>
2024-03-03 13:17:05 -0500

    Update_9

--- index.js
+++ index.js
@@ -2,52 +2,104 @@ const express = require('express') //Importo la libreria
 const app = express() //Inicializacion de la variable que usara la libreria
 const router = express.Router(); // Enrutar los servicios web
 const port = 3000; // Escuchar la ejecucion del servidor
-require('dotenv').config() // Obetenmos las variables de entorno
-const socket = require('socket.io') // Importamos la libreria socket.io
-const http = require('http').Server(app)
-const io = socket(http)
 
 const DB_URL = process.env.DB_URL || '';
+
 const mongoose = require('mongoose'); // Importo la libreria mongoose
-mongoose.connect("mongodb+srv://ferneyrj573:kHlD36izYmhm5ZbX@express.2uyl5nb.mongodb.net/talentotech") // Creo la cadena de conexion
+mongoose.connect("mongodb+srv://ferneyrj573:kHlD36izYmhm5ZbX@express.2uyl5nb.mongodb.net/talentotech")
+const MessageSchema = require('./models/User');
 
-const userRoutes = require('./routes/UserRoutes');
-const houseRoutes = require('./routes/HouseRoutes');
+app.use (express.urlencoded({extended:true}))
+app.use (express.json())
 
-//Metodo [GET, POST, PUT, PATCH, DELETE]
-// Nombre del servicio [/]
-router.get('/', (req, res) => {
-    //Informacion a modificar
-    res.send("Hello world")
-})
+//Crear el servicio web
+router.get('/test', (req, res)=>{
 
-io.on('connect', (socket) => {
-    console.log("connected")
-    //Escuchando eventos desde el servidor
-    socket.on('message', (data) => {
-        console.log(data)
-        //Emitimos eventos hacie el cliente
-        socket.emit('message-receipt', {"message": "Mensaje recibido en el servidor"})
+
+router.post('/user', (req, res)=>{
+    //Crear un usuario
+    let user = UserSchema({
+    name: req.body.name,
+    lastnane: req.body. lastname,
+    enail: req.body.email,
+    password: req. body. password,
+    id: req. body. id
+
     })
 
-    socket.on('disconnect', (socket) => {
-        console.log("disconnect")    
+    user. save()
     })
+ 
+
+    //res.send("Hello world")
+})
+
+
+// router.post('/user', (req, res)=>{
+
+//     var user = {
+
+//         "name": req.body.name,
+//         "lastname": req.body.lastname,
+//         "age": req.body.age,
+//     }
+
+//     res.send(user)
+// })
+
+// router.get('/user', (req, res)=>{
+
+    
+//     res.send("Se ejecuto por GET")
+    
+
+// })
+
+// router.post('/user', (req, res)=>{
+
+    
+//     res.send("Se ejecuto por POST")
+    
+
+// })
+
+router.put('/user', (req, res)=>{
+
+    
+    res.send("Se ejecuto por PUT")
+    
+
+})
+
+router.patch('/user', (req, res)=>{
+
+    
+    res.send("Se ejecuto por PATCH")
+    
+
+})
+
+router.delete('/user', (req, res)=>{
+
+    
+    res.send("Se ejecuto por DELETE")
+    
+
+})
+
+router.options('/user', (req, res)=>{
+
+    
+    res.send("Se ejecuto por OPTIONS")
+    
+
 })
 
 
 
-app.use(express.urlencoded({extended: true})) // Acceder a la informacion de las urls
-app.use(express.json()) // Analizar informacion en formato JSON
-app.use((req, res, next) => {
-    res.io = io
-    next()
-})
-//Ejecuto el servidor
+//Ejecuto el servisor
 app.use(router)
-app.use('/uploads', express.static('uploads'));
-app.use('/', userRoutes)
-app.use('/', houseRoutes)
-http.listen(port, () => {
-    console.log('Listen on ' + port)
+app.listen(port, () =>{
+console.log('Listen on ' + port)
+
 })
